fe2fb1e895941155dadc79df03b7b66b122b5289
Niek Saarberg <n.saarberg@gmail.com>
2012-03-20 19:02:33 +0100

    Added fixes back, and event test that failed!

--- src/dom/morph.js
+++ src/dom/morph.js
@@ -18,6 +18,9 @@ while( !supportsCSSAnimation && i-- ) {
 	}
 }
 
+// For external modules
+Dom.supportsCSSAnimation = supportsCSSAnimation;
+
 Dom.prototype.morph = function ( to/* after, duration, effect */ ) {
 	
 	// No animation supported, set directly to end styles
@@ -53,16 +56,30 @@ Dom.prototype.morph = function ( to/* after, duration, effect */ ) {
 			// 	break;
 		}
 	}
+
+	// Wtf, this all fails, on all browsers...
+	// if( options.after ) {
+	// 	
+	// 	this.once('webkitAnimationEnd', options.after);
+	// 	this.once('mozAnimationEnd', options.after);
+	// 	this.once('animationEnd', options.after);
+	// 	this.once('animationend', options.after);
+	// 	this.once('mozanimationend', options.after);
+	// }
 	
 	PB.each(options.to, function ( key, value ) {
 		
 		properties += key.replace(/[A-Z]/g, function (m) { return '-'+m.toLowerCase(); })+',';
+		
+		from[key] = me.getStyle( key ) || 0;	// || 0, tmp fix
 	});
 	
 	properties = properties.substr( 0, properties.length-1 );
 	
 	from[transitionProperty] = properties;
 	from[transitionDuration] = options.duration+'s';
+	
+	this.setStyle( from );
 	
 	// Add to styles for next rendering frame
 	setTimeout(function() {
