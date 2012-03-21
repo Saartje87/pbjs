6eed5471b34205b58f7a84faad2f727fe6bee98c
Niek Saarberg <n.saarberg@gmail.com>
2012-03-21 09:55:51 +0100

    Added flash detection. PB.rowser.flash will return version number or false when flash is not detected.

--- src/core/browser.js
+++ src/core/browser.js
@@ -4,7 +4,8 @@
 PB.browser = function (){
 	
 	var ua = navigator.userAgent,
-		info;
+		info,
+		flash;
 	
 	info = {
 		
@@ -19,6 +20,26 @@ PB.browser = function (){
 	info.version = info.isIE
 		? parseFloat(ua.match(/MSIE (\d+\.\d+)/)[1])
 		: parseFloat(ua.match(/(Chrome|Firefox|Version|NokiaBrowser)\/(\d+\.\d+)/)[2]);
+		
+	// Flash detection
+	if( navigator.plugins && navigator.plugins['Shockwave Flash'] ) {
+		
+		flash = navigator.plugins['Shockwave Flash'].description;
+	} else if ( window.ActiveXObject ) {
+		
+		try {
+			
+			flash = new ActiveXObject('ShockwaveFlash.ShockwaveFlash').GetVariable('$version');
+		} catch (e) {}
+	}
+	
+	if( flash ) {
+		
+		flash = flash.match(/\d+/g);
+		flash = Number(flash[0]+'.'+flash[1]);
+	}
+	
+	info.flash = flash || false;
 	
 	return info;
 }();
